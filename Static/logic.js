fc7bf7eea0154958c3be1f8d50edb9879e63b193
bholties <77474724+bholties@users.noreply.github.com>
2021-06-10 15:58:50 -0500

    create and define base maps

--- Static/logic.js
+++ Static/logic.js
@@ -1,6 +1,7 @@
 // retrieve earthquake geoJSON data.
 var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+// Satellite background
 var satellite_map = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -8,14 +9,31 @@ var satellite_map = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/ti
     accessToken: API_KEY
 });
 
+// grayscale background
 var graysacle_map = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "grayscale_map",
     accessToken: API_KEY
 
-    var outdoors_map = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// outdoors background
+var outdoors_map = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "outdoors_map",
     accessToken: API_KEY
+
+// Create map object and set default layers
+var map = L.map("mapid", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [satelite_map, grayscale_map, outdoors_map]
+      });
+
+// Define baseMaps Object to Hold Base Layers
+var baseMaps = {
+    "Satellite": satellite_map,
+    "Grayscale": grayscale_map,
+    "Outdoors": outdoors_map
+};
+
